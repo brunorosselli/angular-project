d2ade6ee71662c66713fef7c8162f11f8d6d5576
brunorosselli <bruno.rosselli@hotmail.com>
2022-12-09 17:47:07 -0300

    Create a Simple NG FOR

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+jobs = [
+  {
+    "name": "Wipro",
+    "start": "01/08/2021",
+    "finish": "At Moment"
+  },
+  {
+    "name": "Lynk",
+    "start": "01/03/2021",
+    "finish": "01/08/2021"
+  }
+]
+
+ngOnInit(): void {
+  
+}
 
 }
